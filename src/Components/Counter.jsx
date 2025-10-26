16b2f3a194704fedd2674ff47933e74532139062
Joshua Glaser <jdglaser@usf.edu>
2025-10-26 12:25:46 -0400

    added props and accessability

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,12 +1,16 @@
 'use client'
 import{ useState} from 'react'
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+// Here I added the props
+export default function Counter({initialCount = 0, initialStep = 1}) {
+  const [count, setCount] = useState(initialCount)
+  const [step, setStep] = useState(initialStep)
 
+  // Below i added the aria accessability and the buttons
   return (
     <div>
-      <h2>Count: {count}</h2>
+      <h2 aria-live="polite"> {count} Count: {count}</h2>
+
       <button onClick={() => setCount(count + 1)}>Add one</button>
       <button onClick={() => setCount(count - 1)}>Minus One </button>
       <button onClick={() => setCount(0)}>Reset</button>
